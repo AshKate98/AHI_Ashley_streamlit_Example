dcf2ac1ad23f3bc147b556c40f5e6cf89971e2fb
Ashley <Ashleychinnici25@gmail.com>
2021-12-09 12:19:57 -0500

    Uploading Title

---
+++ AHI_Streamlite_example.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  9 12:03:29 2021
+
+@author: ashle
+"""
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('Hospital Data')
