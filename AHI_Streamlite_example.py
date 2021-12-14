03741a7d017cd46d7d4638a393c9db428ba7a97a
AshKate98 <89862573+AshKate98@users.noreply.github.com>
2021-12-13 22:44:35 -0500

    Update AHI_Streamlite_example.py

--- AHI_Streamlite_example.py
+++ AHI_Streamlite_example.py
@@ -126,7 +126,7 @@ costs_condition_hospital = df_Inpatient.groupby(['provider_state', 'drg_definiti
 st.header("Costs by Condition and Hospital - Average Total Payments")
 st.dataframe(costs_condition_hospital)
 
-costs_condition_hospital = df_Inpatient.groupby(['average_total_payments', 'drg_definition'])['average_total_payments'].sum().reset_index()
+costs_condition_hospital = df_Inpatient.groupby(['provider_id', 'drg_definition'])['average_total_payments'].sum().reset_index()
 st.header("Costs by Condition and Hospital - Average Total Payments")
 st.dataframe(costs_condition_hospital)
 
