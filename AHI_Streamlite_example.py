ad70d4c77e45c1d3d70052ee0fa681ac977792af
AshKate98 <89862573+AshKate98@users.noreply.github.com>
2021-12-13 19:13:32 -0500

    Update AHI_Streamlite_example.py

--- AHI_Streamlite_example.py
+++ AHI_Streamlite_example.py
@@ -4,17 +4,34 @@ Created on Thu Dec  9 12:03:29 2021
 
 @author: ashle
 """
-!pip install streamlit
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly.figure_factory as ff
-!pip install matplotlib
-import matplotlib.pyplot as plt
+import plotly.express as px
 
-st.title('Hospital Data')
+st.title('Streamlit Final Assingment')
 
-df_Hospital = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/AHI_DataSci_507/main/Deployment_Streamlit/hospital_info.csv')
-df_Outpatient = Outpatient_2015 = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/AHI_DataSci_507/main/Deployment_Streamlit/outpatient_2015.csv')
-df_Inpatient = Inpatient_2015 = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/AHI_DataSci_507/main/Deployment_Streamlit/inpatient_2015.csv')
+@st.cache
+def load_hospitals():
+    df_Hospital = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/AHI_STATS_507/main/Week13_Summary/output/df_hospital.csv')
+    return df_Hospital
+
+@st.cache
+def load_Inpatient():
+    df_Inpatient = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/AHI_STATS_507/main/Week13_Summary/output/df_inpatient.csv')
+    return df_inpatient
+
+@st.cache
+def load_Outpatient():
+    df_Outpatient = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/AHI_STATS_507/main/Week13_Summary/output/df_outpatient_2.csv')
+    return df_outpatient_2
+  
+st.header('Hospital Data')
+st.dataframe(df_Hospital)
+
+st.header('Outpatient Data')
+st.dataframe(df_Outpatient)
+
+st.header('Inpatient Data')
+st.dataframe(df_Inpatient)
 
