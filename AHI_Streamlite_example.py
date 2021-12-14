8d38902f2184782738b50513e1bea517b80aa9f0
AshKate98 <89862573+AshKate98@users.noreply.github.com>
2021-12-13 23:40:13 -0500

    Update AHI_Streamlite_example.py

--- AHI_Streamlite_example.py
+++ AHI_Streamlite_example.py
@@ -126,14 +126,19 @@ st.header('Breakdown of each state with each drg definition with total payments 
 costs_condition_hospital = df_Inpatient.groupby(['provider_state', 'drg_definition'])['average_total_payments'].sum().reset_index()
 st.header("Costs by Condition and Hospital - Average Total Payments")
 st.dataframe(costs_condition_hospital)
+st.markdown('Here we can see the total breakdown of each drg definition by state and average total payments')
+st.markdown('Inpatient Q: What is New york states Top average drg payment?')
+st.markdown('A: Here we can see that for NY NY drg_definition 003 - ECMO OR TRACH W MV >96 HRS OR PDX EXC FACE, MOUTH & NECK W MAJOR had the largest total payment of $5,509,499.3400')
+st.markdown('CA leads the most for total averge discharge payments for 853 - INFECTIOUS & PARASITIC DISEASES W O.R. PROCEDURE W MCC $8,380,247.9500')
+
+st.header('The total average payments for provider id, drg definitions, and each total average payment')
+st.dataframe(costs_condition_hospital)
 
 st.header('Breakdown of ech provider id with drg definiton and average total payments')
 costs_condition_hospital = df_Inpatient.groupby(['provider_id', 'drg_definition'])['average_total_payments'].sum().reset_index()
 st.header("Costs by Condition and Hospital - Average Total Payments")
 st.dataframe(costs_condition_hospital)
 
-st.header('The total average payments for provider id, drg definitions, and each total average payment')
-st.dataframe(costs_condition_hospital)
 
 
 
