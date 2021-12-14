45585353b7cde27601875cd53ea14d38a63fad52
AshKate98 <89862573+AshKate98@users.noreply.github.com>
2021-12-13 23:50:01 -0500

    Update AHI_Streamlite_example.py

--- AHI_Streamlite_example.py
+++ AHI_Streamlite_example.py
@@ -69,7 +69,7 @@ st.dataframe(dataframe_pivot)
 
 State_acute_1=df_Hospital.loc[(df_Hospital["hospital_type"]=="Acute Care Hospitals") & (df_Hospital["hospital_overall_rating"]=="1"),["state"]]
 st.subheader('Hospital Type - Acute Care Hospitals')
-bar1 = state-acute_1['hospital_type'].value_counts().reset_index()
+bar1 = state_acute_1['hospital_type'].value_counts().reset_index()
 st.dataframe(bar1)
 
 st.header('Outpatient Data')
